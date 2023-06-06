b08fadc93bb06759f813bf485c5f46fc95a50384
manju <np05cp4s200010@iic.edu.np>
2023-06-06 14:46:44 +0545

    mysqlclient installed, configured msql db and cretied migrtion-db migrated

--- project_scs/app_scs/models.py
+++ project_scs/app_scs/models.py
@@ -19,19 +19,19 @@ class Course(models.Model):
         db_table= "app_courses"
 
 class Student(models.Model):
-    first_name = models.CharField(max_length= 200)
+    first_name = models.CharField(max_length= 100)
     middle_name = models.CharField(max_length=100, null = True, blank = True)
     last_name = models.CharField(max_length=100)
     email = models.EmailField(unique=True)
     contact = models.CharField(max_length=100)
     gender = models.CharField(max_length=100)
     blood_group = models.CharField(max_length=100)
-    academic_level = models.CharField(max_length=200)
+    academic_level = models.CharField(max_length=100)
     academic_status = models.CharField(max_length=100)
     acdemic_org = models.CharField(max_length=100)
     academic_skill = models.CharField(max_length=200, null = True, blank = True)
     # course = model.models.ForeignKey("app.Model", verbose_name=_(""), on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    course = models.ForeignKey( Course, on_delete=models.CASCADE)
     intake = models.CharField(max_length=100, null=True, blank=True)
     shift = models.CharField(max_length=100, null=True, blank=True)
     remarks = models.CharField(max_length=200)
